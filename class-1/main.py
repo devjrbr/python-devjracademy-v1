043a1e5a6ee7d282b5a6689908b1c7995303c86e
Luiz Cruz <luizcruzdev@gmail.com>
2023-05-04 11:47:30 -0300

    add class 1

---
+++ class-1/main.py
@@ -0,0 +1,12 @@
+# Variáveis e tipos de dados básicos
+
+nome = "Rex" # String (str)
+idade = 1120 # Inteiro (int)
+peso = 6.4 # Float (float)
+foo = False # Booleano (bool)
+
+print("Hello World!")
+print(nome,type(nome))
+print(idade,type(idade))
+print(peso,type(peso))
+print(foo,type(foo))
